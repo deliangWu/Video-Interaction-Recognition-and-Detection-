fd139001ea5394b91b7607814171e51f38ec9494
deliang wu <d.wu@students.uu.nl>
2017-03-27 09:22:11 +0800

    test

--- src/train/c3d_pretrain_on_ucf.py
+++ src/train/c3d_pretrain_on_ucf.py
@@ -38,7 +38,7 @@ def main(_):
     # load the dataset into memory
     # ******************************************************
     ucf_set = ucf.ucf101(frmSize,numOfClasses) 
-    test_x,test_y = ucf_set.loadTest(40) 
+    test_x,test_y = ucf_set.loadTest(30) 
     print('initial testing accuracy ',c3d.evaluate(test_x, test_y, sess))
     ucf_set.loadTrainAll()
    
