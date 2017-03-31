76aa8c3405968c5d1062096b77d1dbe4f9553bfb
deliang wu <d.wu@students.uu.nl>
2017-03-31 09:23:30 +0800

    test

--- src/datasets/test.py
+++ src/datasets/test.py
@@ -7,15 +7,18 @@ import ucf101
 import videoPreProcess as vpp
 
 if __name__ == "__main__":
-    arr = np.random.randn(2,5)
-    print(arr)
-    arrNorm = tf.nn.l2_normalize(arr,dim=1)
-    mean_arr = tf.reduce_mean(arr)
-    print(np.mean(arr))
-    sess = tf.InteractiveSession()
-    with sess.as_default():
-        nr = sess.run(mean_arr)
-    print(arrNorm)
-    print(nr)
+    ph_input = tf.placeholder(dtype=tf.int32, shape=[None, 1])
+    print(ph_input)
+    h = tf.zeros([1, 2], dtype=tf.int32)  # ...or some other tensor of shape [1, 2]
+    print(h)
     
+    # Get the number of rows in the fed value at run-time.
+    ph_num_rows = tf.shape(ph_input)[0]
+    print(ph_num_rows)
     
+    # Makes a `ph_num_rows x 2` matrix, by tiling `h` along the row dimension.
+    h_tiled = tf.tile(h, tf.pack([ph_num_rows, 1]))
+    print(h_tiled)
+    
+    result = tf.concat(1, [ph_input, h_tiled])        
+    print(result)
