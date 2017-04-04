bfb4846da250e0c51e4a39de498480eaa5f65358
deliang wu <d.wu@students.uu.nl>
2017-04-04 10:19:17 +0800

    test

--- src/train/c3d_fullNet.py
+++ src/train/c3d_fullNet.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function
 import numpy as np
 import os
@@ -19,16 +18,10 @@ def main(argv):
     # define the network
     # ***********************************************************
     numOfClasses = 6 
-    frmSize = (112,80,3)
-    with tf.variable_scope('atomic_action_features') as scope:
-        if len(argv) >= 3 and argv[2] == 'unShareFeatureVariable':
-            savePrefix = 'c3d_finetune_on_ut_dual_nets_unShareVars_'
-            log = time.ctime() + ' Finetune the dual-nets model with two independent feature variables on UT-Interaction '
-            c3d = network.C3DNET_2F1C(numOfClasses, frmSize, shareFeatureVariable= False)
-        else:
-            savePrefix = 'c3d_finetune_on_ut_dual_nets_shareVars_'
-            log = time.ctime() + ' Finetune the dual-nets model with two sharing feature variables on UT-Interaction '
-            c3d = network.C3DNET_2F1C(numOfClasses, frmSize)
+    frmSize = [(112,128,3),(112,80,3)]
+    
+    with tf.variable_scope('top') as scope:
+        c3d = network.C3DNET_3F1C(numOfClasses, frmSize)
     # ***********************************************************
     # define session
     # ***********************************************************
@@ -40,46 +33,48 @@ def main(argv):
     # ***********************************************************
     # define the dataset
     # ***********************************************************
-    if len(argv) >= 4 and argv[3] == 'set2':
-        ut_set = ut.ut_interaction_set2_atomic(frmSize)
+    if len(argv) >= 3 and argv[2] == 'set2':
+        ut_set = ut.ut_interaction_set2_ga(frmSize)
         seqRange = range(11,21)
-        savePrefix = savePrefix + 'set2_'
-        log = log + 'set2! \n'
+        savePrefix = 'c3d_fullNet_ut_set2_'
+        log = time.ctime() + ' Train the 3D-ConvNet full Net on UT-Interaction dataset set2 from scratch! \n'
     else:    
-        ut_set = ut.ut_interaction_set1_atomic(frmSize)
+        ut_set = ut.ut_interaction_set1_ga(frmSize)
         seqRange = range(1,11)
-        savePrefix = savePrefix + 'set1_'
-        log = log + 'set1! \n'
-    
-    logName =  savePrefix + common.getDateTime() + '.txt'
-    common.clearFile(logName)
-    common.pAndWf(logName,log)
+        savePrefix = 'c3d_fullNet_ut_set1_'
+        log = time.ctime() + ' Train the 3D-ConvNet full Net on UT-Interaction dataset set1 from scratch! \n'
     
     # ***********************************************************
     # Train and test the network
     # ***********************************************************
+    logName =  savePrefix + common.getDateTime() + '.txt'
+    common.clearFile(logName)
+    common.pAndWf(logName,log)    
     iteration = 4001
     batchSize = 15
     for seq in seqRange:
         with sess.as_default():
             sess.run(initVars)
+        saver_feature_g = tf.train.Saver([tf.get_default_graph().get_tensor_by_name(varName) for varName in common.Vars.feature_g_VarsList])
         saver_feature_a0 = tf.train.Saver([tf.get_default_graph().get_tensor_by_name(varName) for varName in common.Vars.feature_a0_VarsList])
         saver_feature_a1 = tf.train.Saver([tf.get_default_graph().get_tensor_by_name(varName) for varName in common.Vars.feature_a1_VarsList])
-        saver_classifier_2f1c = tf.train.Saver([tf.get_default_graph().get_tensor_by_name(varName) for varName in common.Vars.classifier_sm_2f1c_VarsList])
-        log = '**************************************\n' \
+        saver_classifier = tf.train.Saver([tf.get_default_graph().get_tensor_by_name(varName) for varName in common.Vars.classifier_sm_3f1c_VarsList])
+        #saver_feature_g.restore(sess,join(common.path.variablePath, savePrefix  + str(seq) + '_fg.ckpt'))
+        #saver_classifier.restore(sess,join(common.path.variablePath, savePrefix  + str(seq) + '_c.ckpt'))
+        log = '****************************************\n' \
             + 'current sequence is ' + str(seq)  + '\n' + \
               '****************************************\n'
         common.pAndWf(logName,log)
         ut_set.splitTrainingTesting(seq)
-        test_x0,test_x1,test_y = ut_set.loadTesting()
+        test_x,test_x0, test_x1, test_y = ut_set.loadTesting()
         if len(argv) < 2 or argv[1] == 'train' or argv[1] == 'Train':
             best_accuracy = 0
             anvAccuList = np.zeros((10))
             for i in range(iteration):
-                train_x0,train_x1,train_y = ut_set.loadTrainingBatch(batchSize)
+                train_x,train_x0, train_x1, train_y = ut_set.loadTrainingBatch(batchSize)
                 if i%int(iteration/200) == 0:
-                    train_accuracy = c3d.test(train_x0, train_x1, train_y, sess)
-                    test_accuracy = c3d.test(test_x0, test_x1, test_y, sess)
+                    train_accuracy = c3d.test(train_x, train_x0, train_x1, train_y, sess)
+                    test_accuracy = c3d.test(test_x, test_x0, test_x1, test_y, sess)
                     anvAccuList = np.append(anvAccuList[1:10],test_accuracy)
                     anv_accuracy = np.mean(anvAccuList)
                     if anv_accuracy > best_accuracy:
@@ -87,17 +82,14 @@ def main(argv):
                     log = "step %d, training: %g, testing: %g, anv: %g, best %g \n"%(i, train_accuracy, test_accuracy, anv_accuracy, best_accuracy)
                     common.pAndWf(logName,log)
                     if anv_accuracy == 1 or (i > int(iteration * 0.75) and anv_accuracy >= best_accuracy):
-                        save_path_f0 = saver_feature_a0.save(sess,join(common.path.variablePath, savePrefix + str(seq) +'_fa0.ckpt'))
-                        save_path_f1 = saver_feature_a1.save(sess,join(common.path.variablePath, savePrefix + str(seq) +'_fa1.ckpt'))
-                        save_path_2f1c = saver_classifier_2f1c.save(sess,join(common.path.variablePath, savePrefix + str(seq) +'_2f1c.ckpt'))
                         break
-                c3d.train(train_x0, train_x1, train_y, sess)
-            common.pAndWf(logName,' The training is finished at ' + time.ctime() + ' \n')
+                c3d.train(train_x, train_x0, train_x1, train_y, sess)
+            common.pAndWf(logName,' \n')
         else:
-            variableName = savePrefix + str(seq) + '.ckpt'
-            saver.restore(sess,join(common.path.variablePath, variableName))
+            saver_feature_g.restore(sess,join(common.path.variablePath, savePrefix  + str(seq) + '_fg.ckpt'))
+            saver_classifier.restore(sess,join(common.path.variablePath, savePrefix  + str(seq) + '_c.ckpt'))
             # begin to test
-            test_accuracy = c3d.test(test_x0, test_x1, test_y, sess)
+            test_accuracy = c3d.test(test_x, test_y, sess)
             log = "Testing accuracy %g \n"%(test_accuracy)
             common.pAndWf(logName,log)
             
