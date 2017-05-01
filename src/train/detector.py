daf12ebed30543bc4ce5a8be0573fe46e003098d
deliang wu <d.wu@students.uu.nl>
2017-05-01 17:24:49 +0800

    fix bug

--- src/train/detector.py
+++ src/train/detector.py
@@ -62,21 +62,20 @@ def dispIBB(vIn,bbInitFrmNo,ibbList):
         vpp.videoPlay(vplay)
         bbStartFrmNo += 8 
     
-def pred_IBB(video,ibbLists,bbInitFrmNo,sess,c3d): 
+def pred_IBB(video,ibbList,bbInitFrmNo,sess,c3d): 
     pred_yList = []
     bbStartFrmNo = bbInitFrmNo
-    for ibbList in ibbLists:
-        for ibb in ibbList:
-            vChop = video[bbStartFrmNo:bbStartFrmNo+64,ibb[1]:ibb[3],ibb[0]:ibb[2]]
-            vChop = vpp.videoProcessVin(vChop, (112,128,3), downSample = 2, NormEn=True, RLFlipEn=False)
-            vChop_det = np.reshape(vChop,(3,1,16,112,128,3))
-            prob = c3d.evaluateProb(vChop_det, sess)
-            pred_y = np.argmax(prob)
-            top2y = [np.argsort(prob[0])[-1],np.argsort(prob[0])[-2]]
-            if pred_y != 6:
-                pred_yList.append([bbStartFrmNo,ibb,top2y])
-                print(bbStartFrmNo,'+++++++++',ibb,'----- Label is ', top2y)
-        bbStartFrmNo += 8
+    for ibb in ibbList:
+        vChop = video[bbStartFrmNo:bbStartFrmNo+64,ibb[1]:ibb[3],ibb[0]:ibb[2]]
+        vChop = vpp.videoProcessVin(vChop, (112,128,3), downSample = 2, NormEn=True, RLFlipEn=False)
+        vChop_det = np.reshape(vChop,(3,1,16,112,128,3))
+        prob = c3d.evaluateProb(vChop_det, sess)
+        pred_y = np.argmax(prob)
+        top2y = [np.argsort(prob[0])[-1],np.argsort(prob[0])[-2]]
+        if pred_y != 6:
+            pred_yList.append([bbStartFrmNo,ibb,top2y])
+            print(bbStartFrmNo,'+++++++++',ibb,'----- Label is ', top2y)
+    bbStartFrmNo += 8
     return pred_yList
             
 def comb_IBB(pred_yList):           
