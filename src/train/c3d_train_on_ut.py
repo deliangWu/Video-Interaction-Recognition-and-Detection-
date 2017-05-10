3739af957a71fb999b0de33517f2ea71755c196a
deliang wu <d.wu@students.uu.nl>
2017-05-10 13:59:25 +0800

    change the learning rate

--- src/train/c3d_train_on_ut.py
+++ src/train/c3d_train_on_ut.py
@@ -171,7 +171,7 @@ def main(argv):
                     train_x,train_y = ut_set.loadTrainingBatch(batchSize)
                     train_y = ut.oneHot(train_y,numOfClasses)
                     epoch = ut_set.getEpoch()
-                    learning_rate = 0.0002 * 2**(-int(epoch/4))
+                    learning_rate = 0.0002 * 2**(-int(epoch/8))
                     c3d.train(train_x, train_y, sess, learning_rate=learning_rate)
                     #loss = c3d.getLoss(train_x, train_y, sess)
                     #print('step: %d, loss: %g '%(i,loss))
