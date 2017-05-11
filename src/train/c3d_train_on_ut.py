fd131f517ac9136db044aa7cc1699312adf64697
deliang wu <d.wu@students.uu.nl>
2017-05-11 08:25:33 +0800

    test

--- src/train/c3d_train_on_ut.py
+++ src/train/c3d_train_on_ut.py
@@ -134,7 +134,8 @@ def main(argv):
         log = time.ctime() + ' Train the 3D-ConvNet on UT-Interaction dataset set2 from scratch! \n'
     else:    
         ut_set = ut.ut_interaction_set1(frmSize,numOfClasses=numOfClasses)
-        seqRange = range(1,11)
+        #seqRange = range(1,11)
+        seqRange = (1,8)
         savePrefix = 'c3d_train_on_ut_set1_'
         log = time.ctime() + ' Train the 3D-ConvNet on UT-Interaction dataset set1 from scratch! \n'
     
