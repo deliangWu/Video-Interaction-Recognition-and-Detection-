8f5efa8b40c7d2f016df7ce3fa0794e508eeb27a
deliang wu <d.wu@students.uu.nl>
2017-05-18 13:07:15 +0800

    adjust learning rate for detector

--- src/train/c3d_train_on_ut.py
+++ src/train/c3d_train_on_ut.py
@@ -80,7 +80,7 @@ def main(argv):
                     train_x,train_y = ut_set.loadTrainingBatch(batchSize)
                     epoch = ut_set.getEpoch()
                     #learning_rate = 0.0001 * 2**(-int(epoch/8))
-                    learning_rate = 0.005 * 2**(-int(epoch/4))
+                    learning_rate = 0.0001 * 2**(-int(epoch/4))
                     #learning_rate = 0.0001
                     c3d.train(train_x, train_y, sess, learning_rate=learning_rate)
                     #loss = c3d.getLoss(train_x, train_y, sess)
