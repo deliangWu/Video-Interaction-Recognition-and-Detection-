b9192ca74a6edf7d96887d0a904119985148b187
deliang wu <d.wu@students.uu.nl>
2017-06-13 19:49:16 +0800

    detector debug

--- src/train/detector.py
+++ src/train/detector.py
@@ -295,9 +295,9 @@ def main(argv):
     savePrefix = 'c3d_detector_'
     logName =  savePrefix + common.getDateTime() + '.txt'
     common.clearFile(logName)
-    #seqNo = int(argv[1][3:])
-    #seqRange = (seqNo,)
-    seqRange = (1,)
+    seqNo = int(argv[1][3:])
+    seqRange = (seqNo,)
+    #seqRange = (1,)
     for seq in seqRange:
         log = '****************************************\n' \
             + 'current sequence is ' + str(seq)  + '\n' + \
