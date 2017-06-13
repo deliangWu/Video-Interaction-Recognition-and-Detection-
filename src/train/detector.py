0486afd2a78c89404420bb14473a0d738b6af746
deliang wu <d.wu@students.uu.nl>
2017-06-13 17:22:34 +0800

    detector debug: dpp

--- src/train/detector.py
+++ src/train/detector.py
@@ -203,7 +203,7 @@ def pred_IBB(video,ibbList,bbInitFrmNo,sess,c3d,vLen=64,stride=8):
     bbStartFrmNo = bbInitFrmNo
     for ibb in ibbList:
         vChop = video[bbStartFrmNo:bbStartFrmNo+vLen,ibb[1]:ibb[3],ibb[0]:ibb[2]]
-        vChop = vpp.videoProcessVin(vChop, (112,128,3), downSample=1, RLFlipEn=False,numOfRandomCrop=4)
+        vChop = vpp.videoProcessVin(vChop, (112,128,3), downSample=0, RLFlipEn=False,numOfRandomCrop=1)
         vChop = vpp.videoNorm1(vChop,normMode=1)
         vChop_det = np.reshape(vChop,(-1,1,16,112,128,3))
         prob = c3d.evaluateProb(vChop_det, sess)[0]
