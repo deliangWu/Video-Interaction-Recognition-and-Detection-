a01b78cf4844fa7ecb7ae12f9c4e3dd68f4eac4f
deliang wu <d.wu@students.uu.nl>
2017-06-15 10:04:01 +0800

    detector debug

--- src/detector/sp_int_det.py
+++ src/detector/sp_int_det.py
@@ -242,9 +242,9 @@ def NMS_IBB(ibbSets):
         #ySel = ibbSet[2][int(len(ibbSet[2])/2) - 1: int(len(ibbSet[2])/2 + 2)]
         ySel = ibbSet[2]
         ySet =[]
-        m = len(ySel)//1.6
+        m = int(len(ySel)/1.6)
         s = max(0,m-2)
-        e = min(len(ySel),m+3)
+        e = min(len(ySel-1),m+3)
         for y in ySel[s:e]:
             if y[0] !=6:
                 ySet.append([y[0]]*4)
